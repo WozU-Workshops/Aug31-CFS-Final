e1fb3e1a0d571ed0a619b63a4f8925dd9c582ff5
Cassandra Matos <cassandra@matosmendez.com>
2018-08-31 08:45:39 -0700

    code base upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// create function
+
+    // declare variables needed
+
+    // user innerHTML to print user input on the id card
+
+    // create an empty array
+
+    // push elements into array
+
+    // use for loop to go through array
+
+        // use if statements to check to a condition and perform a specific action based on the result aka true or false
+
+
+
